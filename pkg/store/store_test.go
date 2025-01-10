a1b7311020f657cae55e89addde9254bc8e85989
jakedoublev <jake.vanvorhis@virtru.com>
2025-01-10 14:10:49 -0800

    more tests

--- pkg/store/store_test.go
+++ pkg/store/store_test.go
@@ -3,6 +3,8 @@ package store
 import (
 	"encoding/json"
 	"errors"
+	"os"
+	"path/filepath"
 	"strings"
 	"testing"
 
@@ -92,12 +94,81 @@ func Test_NewMemoryStore(t *testing.T) {
 	require.False(t, store.Exists())
 
 	value := mockStoredValue{
-		Name:      "test",
+		Name:      "test_memory",
 		TestValue: "special_test_value",
 	}
 	err = store.Set(value)
 	require.NoError(t, err)
 	require.True(t, store.Exists())
+
+	data, err := store.Get()
+	require.NoError(t, err)
+	require.NotNil(t, data)
+
+	var storedValue *mockStoredValue
+	err = json.Unmarshal(data, &storedValue)
+	require.NoError(t, err)
+
+	assert.Equal(t, value.Name, storedValue.Name)
+	assert.Equal(t, value.TestValue, storedValue.TestValue)
+}
+
+func Test_NewFileSystemStore_DirectoryProvided(t *testing.T) {
+	testNS := "test_namespace"
+	testKey := "profile"
+
+	dir := t.TempDir()
+	store, err := NewFileStore(testNS, testKey, WithStoreDirectory(dir))
+	require.NoError(t, err)
+	require.NotNil(t, store)
+
+	require.False(t, store.Exists())
+
+	value := mockStoredValue{
+		Name:      "fs_store_test",
+		TestValue: "file_system_stored",
+	}
+	err = store.Set(value)
+	require.NoError(t, err)
+	require.True(t, store.Exists())
+
+	// ensure two files were written to the temp dir
+	files, err := os.ReadDir(dir)
+	require.NoError(t, err)
+	require.Len(t, files, 2)
+
+	// check the written files
+	foundGlobalConfigFile := false
+	foundSingleProfileFile := false
+	for _, file := range files {
+		path := filepath.Join(dir, file.Name())
+		data, err := os.ReadFile(path)
+		require.NoError(t, err)
+		require.NotNil(t, data)
+
+		fileContents := string(data)
+
+		// ensure data is not human readable
+		isEncrypted := !strings.Contains(fileContents, value.Name)
+		assert.True(t, isEncrypted)
+		isEncrypted = !strings.Contains(fileContents, value.TestValue)
+		assert.True(t, isEncrypted)
+
+		// assert file name matches URN
+		split := strings.Split(file.Name(), ":")
+		assert.Equal(t, testNS, split[2])
+
+		last := split[len(split)-1]
+		if strings.Contains(last, testKey+".nfo") {
+			foundGlobalConfigFile = true
+		}
+		if strings.Contains(last, testKey+".enc") {
+			foundSingleProfileFile = true
+		}
+	}
+
+	assert.True(t, foundGlobalConfigFile)
+	assert.True(t, foundSingleProfileFile)
 
 	data, err := store.Get()
 	require.NoError(t, err)
