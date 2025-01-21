1b18bde872cffaf3e421ab3af449e31ddfb3df1a
jakedoublev <jake.vanvorhis@virtru.com>
2025-01-21 14:10:26 -0800

    feat(core): enhancements to platform lib interface and implementers

--- pkg/platform/platform.go
+++ pkg/platform/platform.go
@@ -4,11 +4,15 @@ type Platform interface {
 	// Get the username as known to the operating system
 	GetUsername() string
 	// Get the user's home directory
-	GetUserHomeDir() string
-	// Get the data directory for the platform
-	GetDataDirectory() string
-	// Get the config directory for the platform
-	GetConfigDirectory() string
+	UserHomeDir() string
+	// Get the namespaced user-level data directory for the platform
+	UserAppDataDirectory() string
+	// Get the namespaced user-level config directory for the platform
+	UserAppConfigDirectory() string
+	// Get the namespaced system-level data directory for the platform
+	SystemAppDataDirectory() string
+	// Get the namespaced system-level config directory for the platform
+	SystemAppConfigDirectory() string
 }
 
 // NewPlatform creates a new platform object based on the current operating system
--- pkg/platform/platform_test.go
+++ pkg/platform/platform_test.go
@@ -19,12 +19,25 @@ func Test_PlatformMacOS(t *testing.T) {
 	require.True(t, ok)
 	require.NotNil(t, darwin)
 
-	configDir := darwin.GetConfigDirectory()
+	// user scoped
+	configDir := darwin.UserAppConfigDirectory()
 	assert.True(t, strings.HasSuffix(configDir, fakeAppName))
+	assert.True(t, strings.Contains(configDir, "/Library/Application Support"))
+	assert.False(t, strings.HasPrefix(configDir, "/Library/Application Support"))
 
-	dataDir := darwin.GetDataDirectory()
+	dataDir := darwin.UserAppDataDirectory()
 	assert.True(t, strings.HasSuffix(dataDir, fakeAppName))
-	assert.True(t, strings.Contains(dataDir, "Library/Application Support"))
+	assert.True(t, strings.Contains(dataDir, "/Library/Application Support"))
+	assert.False(t, strings.HasPrefix(configDir, "/Library/Application Support"))
+
+	// system scoped
+	configDir = darwin.SystemAppConfigDirectory()
+	assert.True(t, strings.HasSuffix(configDir, fakeAppName))
+	assert.True(t, strings.HasPrefix(configDir, "/Library/Application Support"))
+
+	dataDir = darwin.SystemAppDataDirectory()
+	assert.True(t, strings.HasSuffix(dataDir, fakeAppName))
+	assert.True(t, strings.HasPrefix(dataDir, "/Library/Application Support"))
 }
 
 func Test_PlatformLinux(t *testing.T) {
@@ -38,16 +51,41 @@ func Test_PlatformLinux(t *testing.T) {
 	require.True(t, ok)
 	require.NotNil(t, linux)
 
-	configDir := linux.GetConfigDirectory()
+	// user scoped
+	configDir := linux.UserAppConfigDirectory()
 	assert.True(t, strings.HasSuffix(configDir, fakeAppName))
-	assert.True(t, strings.Contains(configDir, ".config"))
+	assert.True(t, strings.Contains(configDir, "/.config"))
+	assert.False(t, strings.HasPrefix(configDir, "/.config"))
+	assert.False(t, strings.HasPrefix(configDir, "/etc"))
 
-	dataDir := linux.GetDataDirectory()
+	dataDir := linux.UserAppDataDirectory()
 	assert.True(t, strings.HasSuffix(dataDir, fakeAppName))
-	assert.True(t, strings.Contains(dataDir, ".local/share"))
+	assert.True(t, strings.Contains(dataDir, "/.local/share"))
+	assert.False(t, strings.HasPrefix(dataDir, "/var/lib"))
+	assert.False(t, strings.HasPrefix(dataDir, "/.local/share"))
+
+	// system scoped
+	configDir = linux.SystemAppConfigDirectory()
+	assert.True(t, strings.HasSuffix(configDir, fakeAppName))
+	assert.True(t, strings.HasPrefix(configDir, "/etc"))
+
+	dataDir = linux.SystemAppDataDirectory()
+	assert.True(t, strings.HasSuffix(dataDir, fakeAppName))
+	assert.True(t, strings.HasPrefix(dataDir, "/var/lib"))
+}
+
+// test utility to convert path to windows format and simplify cross-platform testing
+func convertToWindowsPath(path string) string {
+	return strings.ReplaceAll(path, "/", `\`)
 }
 
 func Test_PlatformWindows(t *testing.T) {
+	fakeDrive := "FakeCDrive:\\"
+	// set up fake environment variables
+	t.Setenv(envKeyLocalAppData, fakeDrive+"Users\\test\\AppData\\Local")
+	t.Setenv(envKeyProgramData, fakeDrive+"ProgramData")
+	t.Setenv(envKeyProgramFiles, fakeDrive+"ProgramFiles")
+
 	fakeAppName := "test-windows-app"
 	plat, err := NewPlatform(fakeAppName, "windows")
 
@@ -58,8 +96,25 @@ func Test_PlatformWindows(t *testing.T) {
 	require.True(t, ok)
 	require.NotNil(t, windows)
 
-	configDir := windows.GetConfigDirectory()
+	// user scoped
+	configDir := windows.UserAppConfigDirectory()
 	assert.True(t, strings.HasSuffix(configDir, fakeAppName))
+	assert.True(t, strings.Contains(configDir, "AppData\\Local"))
+	assert.True(t, strings.HasPrefix(configDir, fakeDrive))
 
-	// TODO: tests according to different OS versions
+	dataDir := windows.UserAppDataDirectory()
+	assert.True(t, strings.HasSuffix(dataDir, fakeAppName))
+	assert.True(t, strings.Contains(dataDir, "AppData\\Local"))
+	assert.True(t, strings.HasPrefix(dataDir, fakeDrive))
+
+	// system scoped
+	configDir = windows.SystemAppConfigDirectory()
+	configDir = convertToWindowsPath(configDir)
+	assert.True(t, strings.HasSuffix(configDir, fakeAppName))
+	assert.True(t, strings.HasPrefix(configDir, fakeDrive+"ProgramFiles\\"))
+
+	dataDir = windows.SystemAppDataDirectory()
+	dataDir = convertToWindowsPath(dataDir)
+	assert.True(t, strings.HasSuffix(dataDir, fakeAppName))
+	assert.True(t, strings.HasPrefix(dataDir, fakeDrive+"ProgramData\\"))
 }
